860bf0fd9a665f998c2030d3d9ae6e95e34f7a14
Heinz <hlanaumann@gmail.com>
2022-04-12 07:04:13 +0200

    Add the spent and the budget in the local storage

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,14 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg'
 import { generarId } from './helpers'
 
 function App() {
-	const [gastos, setGastos] = useState([])
-	const [presupuesto, setPresupuesto] = useState(0)
+	const [gastos, setGastos] = useState(
+		localStorage.getItem('gastos')
+			? JSON.parse(localStorage.getItem('gastos'))
+			: []
+	)
+	const [presupuesto, setPresupuesto] = useState(
+		localStorage.getItem('presupuesto') ?? 0
+	)
 	const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 	const [modal, setModal] = useState(false)
 	const [animarModal, setAnimarModal] = useState(false)
@@ -22,6 +28,22 @@ function App() {
 			}, 500)
 		}
 	}, [gastoEditar])
+
+	useEffect(() => {
+		localStorage.setItem('gastos', JSON.stringify(gastos) ?? [])
+	}, [gastos])
+
+	useEffect(() => {
+		Number(localStorage.setItem('presupuesto', presupuesto)) ?? 0
+	}, [presupuesto])
+
+	useEffect(() => {
+		const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0
+
+		if (presupuestoLS > 0) {
+			setIsValidPresupuesto(true)
+		}
+	}, [])
 
 	const handleNuevoGasto = () => {
 		setModal(true)
